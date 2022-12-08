d7115f5bc373235c3ac640cabc393c42952a558e
Majid Abu Rmelah <majid.aburmelah@hotmail.com>
2022-12-08 12:16:54 +0200

    Sync Update

--- src/constants/APICollection.js
+++ src/constants/APICollection.js
@@ -1,10 +1,11 @@
 import API from "../models/API";
 import APITypeEnum from "./APITypeEnum";
 
-const AddDog = new API("http://localhost:8080/dog", APITypeEnum.POST);
-const UpdateDog = new API("http://localhost:8080/dog", APITypeEnum.PUT);
-const GetDogs = new API("http://localhost:8080/dogs", APITypeEnum.GET);
-const GetDog = new API("http://localhost:8080/dog", APITypeEnum.GET);
-const DeleteDog = new API("http://localhost:8080/dog", APITypeEnum.DELETE);
+const EndPoint = "https://dogsinfomanager-backend-workload-eb3js8axwwqyy.cpln.app";
+const AddDog = new API(EndPoint + "/dog", APITypeEnum.POST);
+const UpdateDog = new API(EndPoint + "/dog", APITypeEnum.PUT);
+const GetDogs = new API(EndPoint + "/dogs", APITypeEnum.GET);
+const GetDog = new API(EndPoint + "/dog", APITypeEnum.GET);
+const DeleteDog = new API(EndPoint + "/dog", APITypeEnum.DELETE);
 
 export { AddDog, UpdateDog, GetDogs, GetDog, DeleteDog }
